70630a88f990bf30105ef5a9d97280fc9de8870e
Joseph Robinson <smartech56@gmail.com>
2019-11-07 17:55:31 -0800

    Added files

---
+++ Assets/_DONT_USE_/Editor/Movement/FollowPlayerInspector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(FollowPlayer))]
+public class FollowPlayerInspector : BaseInspectorWindow
+{
+	private string explanation = "This gameObject will pursue the Player constantly.";
+
+	public override void OnInspectorGUI()
+	{
+		GUILayout.Space(10);
+		EditorGUILayout.HelpBox(explanation, MessageType.Info);
+
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("targetPlayer"));
+
+		//Draw custom inspector
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("speed"));
+
+		GUILayout.Space(10);
+
+		SerializedProperty lookAtPlayerProperty = serializedObject.FindProperty("lookAtPlayer");
+
+		lookAtPlayerProperty.boolValue = EditorGUILayout.BeginToggleGroup("Look at Player", lookAtPlayerProperty.boolValue);
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("useSide"));
+		EditorGUILayout.EndToggleGroup();
+
+		if (GUI.changed)
+		{
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
